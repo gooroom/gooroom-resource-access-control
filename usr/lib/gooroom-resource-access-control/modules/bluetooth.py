77a55cf28c13ab14eb30146ef74ce865300a80a7
hmkim <hmkim@gooroom.kr>
2020-02-05 16:07:59 +0900

    fixed bluetooth mac address bug
    Change-Id: Ica864026b1dc2a438e56cd9d6f9f42cb97b9fcdf

--- usr/lib/gooroom-resource-access-control/modules/bluetooth.py
+++ usr/lib/gooroom-resource-access-control/modules/bluetooth.py
@@ -22,7 +22,33 @@ def do_task(param, data_center):
 
     try:
         mode = param[0]
-        mac = param[1].replace('.', ':').strip('\n').upper()
+        unique = param[1].strip() if param[1] else param[1]
+        name = param[2].strip() if param[2] else param[2]
+        mac = ''
+        if unique:
+            mac = unique
+        elif name:
+            name = name.upper()
+            delim_cnt = 0
+            num_cnt = 0
+            for n in name:
+                if n == '.' or n == ':':
+                    delim_cnt += 1
+                    continue
+                if ord(n) >= ord('0') and ord(n) <= ord('9'):
+                    num_cnt += 1
+                    continue
+                if ord(n) >= ord('A') and ord(n) <= ord('F'):
+                    num_cnt += 1
+                    continue
+                break
+            else:
+                if (delim_cnt == 0 or delim_cnt == 5) and num_cnt == 12 :
+                    mac = name
+        if not mac:
+            raise Exception('!! bluetooth mac not found')
+
+        mac = mac.replace('.', ':').strip('\n').upper()
         for m in data_center.get_bluetooth_whitelist():
             if m.upper() == mac:
                 logger.info('mac({}) is in whitelist'.format(mac))
