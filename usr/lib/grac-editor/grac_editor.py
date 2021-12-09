bd3192c355b18cec39323e2f08351d7205f4d6c0
hmkim <hmkim@gooroom.kr>
2021-12-09 19:46:33 +0900

    re-allow clipboard/screencapture
    Change-Id: I42554b86d781db06cfea80c7760d767a24c141c2

--- usr/lib/grac-editor/grac_editor.py
+++ usr/lib/grac-editor/grac_editor.py
@@ -169,8 +169,8 @@ class GracEditor:
                     self.write_txtvw('txtvw_iptables', txt)
                     
             #TEMPORARY
-            self.builder.get_object('swtch+clipboard').set_sensitive(False)
-            self.builder.get_object('swtch+screen_capture').set_sensitive(False)
+            #self.builder.get_object('swtch+clipboard').set_sensitive(False)
+            #self.builder.get_object('swtch+screen_capture').set_sensitive(False)
                 
     def load_rules(self, rule_path):
         """
